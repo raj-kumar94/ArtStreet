0455ee2b16163b3234b47bb91e32bd3ae3e392aa
Raj Kumar <rcrazy88.007@gmail.com>
2017-03-24 14:17:58 +0530

    lilltle change in migration files
    seeding: run VoyagerDatabaseSeeder.php then
    VoyagerDummyDatabaseSeeder.php
    
    php artisan db:seed --class="className"

--- database/migrations/2017_03_19_111936_create_arts_table.php
+++ database/migrations/2017_03_19_111936_create_arts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateArtPostsTable extends Migration
+class CreateArtsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
                 'password'       => bcrypt('password'),
+				'gender'         => 1,
                 'remember_token' => str_random(60),
                 'role_id'        => $role->id,
             ]);
