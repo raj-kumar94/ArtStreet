19fd48bc8e410d97f6f5ba71d8269ec02adbff56
Raj Kumar <rcrazy88.007@gmail.com>
2017-03-24 10:48:59 +0530

    make it better

--- database/migrations/2017_03_19_111936_create_arts_table.php
+++ database/migrations/2017_03_19_111936_create_arts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateArtPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('arts', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->text('content');
@@ -28,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('arts');
     }
 }
